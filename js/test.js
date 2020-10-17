bbf68cf9ac3aed338377e2daf11654f48affcc6d
AhmedKamal148 <a.k1481997@yahoo.com>
2020-10-17 17:55:56 +0200

    'change erorr regex'

--- js/test.js
+++ js/test.js
@@ -1,54 +1,48 @@
-// var time = new Date(),
+let myPaswword = document.querySelector("#my_password"),
+  btnShow = document.querySelector("#show"),
+  icon_Num_chars = document.querySelector("#numChars"),
+  icon_Upper_Lower = document.querySelector("#upper_lower"),
+  icon_digits = document.querySelector("#digits"),
+  informationBtn = document.querySelector("#information"),
+  rulesDiv = document.querySelector(".rules"),
+  min_length,
+  pattern = /[A-Z]/,
+  pattern2 = /\d/;
 
-//   sec = time.getSeconds(),
-//   mins = time.getMinutes(),
-//   div = document.querySelector("div"),
-//   secpass,
-//   countDown = setInterval(function () {
-//     secpass();
-//   }, 1000);
-// // Will Call secpass() Evrey 1s;
-// // ------------------------------------------------------------------------------------------------------
+btnShow.onclick = function () {
+  if (myPaswword.getAttribute("type") == "password") {
+    btnShow.textContent = "Hide";
+    myPaswword.setAttribute("type", "text");
+  } else {
+    btnShow.textContent = "Show";
+    myPaswword.setAttribute("type", "password");
+  }
+};
 
-// function secpass() {
-// //   var min = Math.floor(sec / 60), // 119 / 60 =  1.98
-//     realSec = sec % 60; // calc real seconds
+myPaswword.oninput = function () {
+  // Check length
+  min_length = myPaswword.value.length;
+  if (min_length > 6) {
+    icon_Num_chars.style.color = "green";
+  } else {
+    icon_Num_chars.style.color = "red";
+  }
 
-//   if (sec < 10) {
-//     realSec = "0" + realSec; // Put '0' Before sec
-//   }
-//   if(mins < 10 )
-//   {
-//     mins = "0" + mins;
-//   }
+  if (pattern.test(myPaswword.value)) {
+    icon_Upper_Lower.style.color = "green";
+  } else {
+    icon_Upper_Lower.style.color = "red";
+  }
+  if (pattern2.test(myPaswword.value)) {
+    icon_digits.style.color = "green";
+  } else {
+    icon_digits.style.color = "red";
+  }
+};
 
-//   div.innerHTML =  mins + ":" + realSec; // Show Value into div
-// // ------------------------------------------------------------------------------------------------------
-//   if (mins !== 0)  { // if minutes Not Equal 0
-//     sec--; // minus 1  from Seconds
-//     if (sec == 0)  { // if Seconds Not Equal 0
-//       mins--;  // minus 1  from Minutes
-//       sec = 59; 
-//     }
-// // ------------------------------------------------------------------------------------------------------
-
-//   } else { // If Minutes = 0
-//     if (mins == 0) { // If Minutes = 0
-//       sec--; // minus 1  from Seconds
-// // ------------------------------------------------------------------------------------------------------
-
-//       {
-//         if (sec == 0) {
-//           setTimeout(function () { // Used setTimeOut cause when second = 0 The Value into div being 01 , so i used it for waitn one second more to clearInterval
-//             clearInterval(countDown);
-//             div.textContent = "done..";
-//           }, 1000);
-//         }
-// // ------------------------------------------------------------------------------------------------------
-
-//       }
-//     }
-//   }
-// }
-
-// secpass();
+window.onload = function () {
+  $(rulesDiv).hide();
+};
+informationBtn.onclick = function () {
+  $(rulesDiv).toggle(1000);
+};
